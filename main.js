ef23c977ac474749da71942efdabb032e7e21d43
sipeilenuo <94128105+sipeilenuo@users.noreply.github.com>
2026-02-24 17:59:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function rolechoose(button) {
   button.classList.toggle('active');
-  button.classList.toggle('visible-false');
   
   console.log('role:', button.dataset.role);
 }
+
+function tkrb_vis(button) {
+  const tkrblist = document.getElementById("alltkrb");
+  tkrblist.classList.toggle('visible-off');
+  
+  button.classList.toggle('active');
+  button.textContent = isHidden ? "顯示" : "隱藏";
+}
+
 
 
